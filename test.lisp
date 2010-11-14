a8184af1e40778ba61a31dacd19e7a47ca580464
Philip Munksgaard <pmunksgaard@gmail.com>
2010-11-14 15:12:23 +0100

    blaba

--- test.lisp
+++ test.lisp
@@ -1,6 +1,8 @@
-(defstruct field
+(defstruct hexagon
+  x ; få lavet alias ting
+  y
   (color 'white)
-  (border 'black))
+  (border-color 'black))
 
 (defun print-hexagon (h stream depth)
   (format stream "#<~A,~A>" (hexagon-x h) (hexagon-y h)))
@@ -29,7 +31,68 @@
 (defun down-left (coords)
   (cons (1- (x coords)) (1+ (y coords))))
 
-(defun get-field (coords hexgrid)
-  (nth (y coords) (nth (x coords) hexgrid)))
+(defun tip-height (side-length)
+  (sqrt (- (expt side-length 2) (expt (/ side-length 2) 2))))
 
+(defun tip-base (side-length)
+  (sqrt (- (expt side-length 2) (expt (tip-height side-length) 2))))
+
+(defun vertices-pixel-coords (a-offset side-length)
+  (let* ((h (tip-height side-length))
+         (j (tip-base side-length)))
+    (list a-offset 
+          (cons (+ (x a-offset) j) (- (y a-offset) h))
+          (cons (+ (x a-offset) j side-length) (- (y a-offset) h))
+          (cons (+ (x a-offset) (* 2 j) side-length) (y a-offset))
+          (cons (+ (x a-offset) j side-length) (+ (y a-offset) h))
+          (cons (+ (x a-offset) j) (+ (y a-offset) h))
+          (cons (+ (x a-offset) j) (+ (y a-offset) h)))))
+
+(defun hexagon-pixel-offset (hexagon side-length)
+  (let ((h (tip-height side-length))
+        (j (tip-base side-length)))
+    (cons (* (+ side-length (- side-length j)) (hexagon-x hexagon))
+          (+ (* 2 h (hexagon-y hexagon)) 
+             (* (1+ (hexagon-x hexagon)) h)))))
+         
+(defun vertices->pixelpairs (vertices)
+  (mapcar #'pair->string vertices))
+
+(defun pair->string (ls)
+  (format nil "~d,~d" (car ls) (cdr ls)))
+
+(defun list->string (ls)
+  (reduce #'(lambda (x y) (concatenate 'string x " " y)) ls))
+
+(defun svg-header ()
+  (princ  "<?xml version=\"1.0\" standalone=\"no\"?>\
+<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" 
+\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\
+\
+<svg width=\"100%\" height=\"100%\" version=\"1.1\"\
+xmlns=\"http://www.w3.org/2000/svg\">")
+  (terpri))
+
+(defun svg-hexagon (hexagon side-length)
+  (princ "<polygon points=\"")
+  (princ (list->string (vertices->pixelpairs 
+                        (vertices-pixel-coords 
+                         (hexagon-pixel-offset hexagon side-length)
+                         side-length))))
+  (format t "\" style=\"fill:~A; stroke:~A; stroke-width:2\" />" 
+          (hexagon-color hexagon) 
+          (hexagon-border-color hexagon)))
+
+(defun svg-hexagons (hexagons side-length)
+  (mapc #'(lambda (x) (progn 
+                         (svg-hexagon x side-length)
+                         (terpri))) hexagons))
+
+(defun svg-footer ()
+  (princ "</svg>"))
+
+(defun hexagons->svg (hexs side-length)
+  (svg-header)
+  (svg-hexagons hexs side-length)
+  (svg-footer))
 
